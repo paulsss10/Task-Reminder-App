93842a4f024b2ef11d20df31168450ecd566f945
jsumande <62502528+paulsss10@users.noreply.github.com>
2021-05-14 23:58:56 +0800

    slight UI changes with tasks styling

--- Frontend/src/Components/TaskComponents/Tasks.js
+++ Frontend/src/Components/TaskComponents/Tasks.js
@@ -8,6 +8,10 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import { FaTrashAlt, FaExpandAlt } from 'react-icons/fa';
 
 const Tasks = () => {
   const classes = useStyles();
@@ -39,7 +43,28 @@ const Tasks = () => {
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small">Expand Details</Button>
+                  <Tooltip
+                    title="Expand Details"
+                    placement="bottom"
+                    aria-label="expand-details"
+                  >
+                    <IconButton
+                      aria-label="expand-task"
+                      className={classes.margin}
+                    >
+                      <FaExpandAlt fontSize="small" />
+                    </IconButton>
+                  </Tooltip>
+
+                  <Tooltip
+                    title="Scrap this task"
+                    placement="bottom"
+                    aria-label="delete-details"
+                  >
+                    <IconButton aria-label="delete" className={classes.margin}>
+                      <FaTrashAlt fontSize="small" />
+                    </IconButton>
+                  </Tooltip>
                 </CardActions>
               </Card>
             );
@@ -51,6 +76,9 @@ const Tasks = () => {
 };
 
 const useStyles = makeStyles({
+    root: {
+        marginBottom: "1.1em"
+    },
   bullet: {
     display: "inline-block",
     margin: "0 2px",
